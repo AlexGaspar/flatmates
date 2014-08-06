9cd08fad942b3789b8402e54eba0a623331ec89a
Alex <al.gaspar@gmail.com>
2014-08-07 01:52:59 +0200

    Init sails

---
+++ api/controllers/AdsController.js
@@ -0,0 +1,13 @@
+/**
+ * AdsController
+ *
+ * @description :: Server-side logic for managing ads
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	'index' : function(req, res, next) {
+        return res.send('Hello world!');
+    }
+};
+
