0418b495e5f08c151a6fa46f39592d615441839e
Alex <al.gaspar@gmail.com>
2014-10-21 01:18:06 +0200

    Only draw a circle if we have an address

--- assets/js/modules/details.js
+++ assets/js/modules/details.js
@@ -1,11 +1,37 @@
 jQuery(document).ready(function($) {
     // Create map
-    getCoordinates($('.map-wrapper').data('address'));
+    generateMap($('.map-wrapper').data('address'));
 });
 
 
-function generateMap(coo) {
-  var center = new google.maps.LatLng(coo.lat, coo.lng);
+function generateMap(address) {
+  if(address) {
+    $.ajax({
+      url: "https://maps.googleapis.com/maps/api/geocode/json",
+      data: {
+        address: address + ' brussels'
+      },
+      dataType: 'JSON',
+      success: function(res) {
+        if(res && res.results[0]) {
+          return initializeMap(
+            res.results[0].geometry.location
+          );
+        }
+      }
+    });
+  } else {
+    initializeMap();
+  }
+};
+
+function initializeMap(coo) {
+  var coordinates = coo || {
+    lat: 50.8503396,
+    lng: 4.3517103
+  };
+
+  var center = new google.maps.LatLng(coordinates.lat, coordinates.lng);
 
   // create map
   var mapOptions = {
@@ -19,35 +45,17 @@ function generateMap(coo) {
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
 
-  // Create circle
-  var populationOptions = {
-    strokeColor: '#257B6E',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#60bdaf',
-    fillOpacity: 0.35,
-    map: map,
-    center: center,
-    radius: 400
-  };
-
-  cityCircle = new google.maps.Circle(populationOptions);
-}
-
-function getCoordinates(address) {
-  // Transform address into coordinates
-  $.ajax({
-    url: "https://maps.googleapis.com/maps/api/geocode/json",
-    data: {
-      address: address + ' brussels'
-    },
-    dataType: 'JSON',
-    success: function(res) {
-      if(res && res.results[0]) {
-        return generateMap(
-          res.results[0].geometry.location
-        );
-      }
-    }
-  });
-}
+  if(coo) {
+    // Create circle
+    new google.maps.Circle({
+      strokeColor: '#257B6E',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#60bdaf',
+      fillOpacity: 0.35,
+      map: map,
+      center: center,
+      radius: 400
+    });
+  }
+};
